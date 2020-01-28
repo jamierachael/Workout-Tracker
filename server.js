47c64f8351111d6f3b5e7a8d035d7117800bd7ee
jamierachael <jamie.rachael@gmail.com>
2020-01-28 18:35:32 -0500

    added database

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 const db = require("./models");
 
 const app = express();
+const databaseName = "workout"
 
 app.use(logger("dev"));
 
@@ -15,57 +16,58 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-db.Library.create({ name: "Campus Library" })
-    .then(dbLibrary => {
-        console.log(dbLibrary);
-    })
-    .catch(({ message }) => {
-        console.log(message);
-    });
+// db.Library.create({ name: "Campus Library" })
+//     .then(dbLibrary => {
+//         console.log(dbLibrary);
+//     })
+//     .catch(({ message }) => {
+//         console.log(message);
+//     });
 
-app.post("/submit", ({ body }, res) => {
-    db.Book.create(body)
-        .then(({ _id }) => db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
-        .then(dbLibrary => {
-            res.json(dbLibrary);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
+// app.post("/submit", ({ body }, res) => {
+//     db.Book.create(body)
+//         .then(({ _id }) => db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
+//         .then(dbLibrary => {
+//             res.json(dbLibrary);
+//         })
+//         .catch(err => {
+//             res.json(err);
+//         });
+// });
 
-app.get("/books", (req, res) => {
-    db.Book.find({})
-        .then(dbBook => {
-            res.json(dbBook);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
+// app.get("/books", (req, res) => {
+//     db.Book.find({})
+//         .then(dbBook => {
+//             res.json(dbBook);
+//         })
+//         .catch(err => {
+//             res.json(err);
+//         });
+// });
 
-app.get("/library", (req, res) => {
-    db.Library.find({})
-        .then(dbLibrary => {
-            res.json(dbLibrary);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
+// // can use for "all" maybe
+// app.get("/library", (req, res) => {
+//     db.Library.find({})
+//         .then(dbLibrary => {
+//             res.json(dbLibrary);
+//         })
+//         .catch(err => {
+//             res.json(err);
+//         });
+// });
 
-app.get("/populated", (req, res) => {
-    db.Library.find({})
-        .populate("books")
-        .then(dbLibrary => {
-            res.json(dbLibrary);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
+// app.get("/populated", (req, res) => {
+//     db.Library.find({})
+//         .populate("books")
+//         .then(dbLibrary => {
+//             res.json(dbLibrary);
+//         })
+//         .catch(err => {
+//             res.json(err);
+//         });
+// });
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
