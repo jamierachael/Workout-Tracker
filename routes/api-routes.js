286c085833f37d48be65b7c4b1dd6a66b2530c2b
jamierachael <jamie.rachael@gmail.com>
2020-01-30 22:55:03 -0500

    Updated API routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const db = require("../models");
+// const mongojs = require("mongojs")
 
 
 
@@ -21,73 +22,58 @@ router.get("/workouts", (req, res) => {
         });
 });
 
-
-
-
 //  addExercise PUT /api/workouts/ 
 // Not yet working
+// updating current
+router.put("/workouts/:id", ({ params, body }, res) => {
+    console.log(body);
+    db.Workout.findByIdAndUpdate(
 
-router.put("/workouts/:id", ({ params }, res) => {
-    //     db.Workout.update(
-    //         {
-    //             _id: mongojs.ObjectId(params.id)
-    //         },
-    //         {
-    //             $set: {
-    //                 new: true
-    //             }
-    //         },
+        params.id,
+        {
+            $push: {
+                exercises: body,
 
-    //         (error, edited) => {
-    //             if (error) {
-    //                 console.log(error);
-    //                 res.send(error);
-    //             } else {
-    //                 console.log(edited);
-    //                 res.send(edited);
-    //             }
-    //         }
-    //     );
+            },
+
+        },
+        {
+            new: true,
+            runValidators: true
+        }
+
+    )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 
 });
-
-
-// Test post 
-
-// router.post('/test', function (req, res) {
-//     res.send('Got a POST request')
-// })
 
 // // // createWorkout POST /api/workouts
 // Not yet working  what replaces { new: true }?
 
 router.post("/workouts", ({ body }, res) => {
-    //     db.Workout.create(body)
-    //         .then(({ _id }) => db.Workout.findOneAndUpdate({}, { $push: { workouts: _id } }, { new: true }))
-    //         .then(dbWorkout => {
-    //             res.json(dbWorkout);
-    //         })
-    //         .catch(err => {
-    //             res.json(err);
-    //         });
+    console.log(body);
+    db.Workout.create({})
+        // .then(({ _id }) => db.Workout.findOneAndUpdate({}, { $push: { workouts: _id } }, { new: true }))
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 
     res.send('Got a POST request')
 });
 
 
-
-
-
-
-
-
-
-
-
 // // getWorkoutsInRange() GET /api/workouts/range
 
 router.get("/workouts/range", (req, res) => {
-    db.Workout.find({})
+    db.Workout.find({}).limit(7)
         .then(dbWorkout => {
             res.json(dbWorkout);
         })
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+// const mongojs = require("mongojs");
+
+
 
 const PORT = process.env.PORT || 8080;
 
